963bea6f47f5445ee0dfda62ed6a23244c181886
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2019-02-25 22:44:32 -0500

    removed console logs and added comments to the app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//fetches todos from the local storage database
 getTodos = () => {
     let todos = [];
     let todosStr =  localStorage.getItem('todo');
@@ -8,11 +8,11 @@ getTodos = () => {
     return todos;
 }
 
+//adds todo to the local storage database and then also calls the show() function to display in the UI
 add = () => {
     let todos = getTodos();
     let task = document.getElementById("task")
     todos.push(task.value)
-    console.log(todos);
     
     localStorage.setItem('todo', JSON.stringify(todos));
 
@@ -23,6 +23,8 @@ add = () => {
 
 }
 
+//This function's just is to show the list of tasks to the UI. It also runs a conditional check on the 
+//checkbox inputs to see if they are checked. If they are, the task is marked as complete
 show = () => {
     let todos = getTodos();
     let html = '<h3>to be completed</h3><ul>';
@@ -39,7 +41,6 @@ show = () => {
 
     for (let i = 0; i < checkboxes.length; i++) {
         checkboxes[i].addEventListener("click", function(){
-            // console.log(checkboxes[i].parentElement);
             
            if (checkboxes[i].checked) {
                checkboxes[i].parentElement.style = "text-decoration: line-through;";
@@ -56,13 +57,14 @@ show = () => {
     };
 }
 
+//The remove function handles grabbing the id from the current task and using that as a selector to remove it
+//from the local storage database. Additionally, the todos array is spliced to remove that current task from the 
+//array
 function remove() {
-    console.log(this);
 
     let id = this.getAttribute('id');
     let todos = getTodos();
     
-    console.log("click");
     todos.splice(id, 1);
     localStorage.setItem('todo', JSON.stringify(todos));
 
@@ -70,6 +72,8 @@ function remove() {
 
 }
 
-
+//the intiating event listener for the add button. It starts by running the add() function
 document.getElementById("add").addEventListener("click", add);
+
+//The show function is initially called to automatically display the current tasks in the database to the UI
 show();
