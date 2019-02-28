9635de4ea196d3f765e76e844cca34ba36e454cb
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2019-02-28 09:56:16 -0500

    removed individual delete buttons and instead made one delete all button

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ show = () => {
     let todos = getTodos(); 
     let html = '<h3>to be completed</h3><ul>';
     for(let i = 0; i < todos.length; i++) {
-        html += '<li>' + todos[i] + '<input name="mycheckbox'+i+'" type="checkbox" class="checkBox"><button class="remove" id="' + i  + '">Delete</button></li>';
+        html += '<li>' + todos[i] + '<input name="'+i+'" type="checkbox" class="checkBox"></li>';
         
     };
     html += '</ul>';
@@ -47,10 +47,11 @@ show = () => {
     document.getElementById("todos").innerHTML = html;
 
 
-    var buttons = document.getElementsByClassName("remove");
+    var button = document.getElementById("remove");
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     for (let i = 0; i < checkboxes.length; i++) {
+        
         checkboxes[i].addEventListener("click", function() {
             if (checkboxes[i].checked) {
                 checkboxes[i].parentElement.classList.add("done");
@@ -60,6 +61,7 @@ show = () => {
         })
         
     }
+        
 
     // variable to store our current state
     var cbstate;
@@ -88,11 +90,13 @@ show = () => {
     // Loop through results and ...
     for(var i = 0; i < cb.length; i++) {
 
+        
         //bind click event handler
         cb[i].addEventListener('click', function(evt) {
             
-            // If checkboxe is checked then save to state
+            // If checkbox is checked then save to state
             if (this.checked) {
+                
             cbstate[this.name] = true;
             
             }
@@ -106,24 +110,42 @@ show = () => {
             localStorage.CBState = JSON.stringify(cbstate);
         });
     }
-
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", remove);      
-    };
+    
+    button.addEventListener("click", remove);
 }
 
 //The remove function handles grabbing the id from the current task and using that as a selector to remove it
 //from the local storage database. Additionally, the todos array is spliced to remove that current task from the 
 //array
 function remove() {
-
-    let id = this.getAttribute('id');
     
+    // var cbstate = JSON.parse(localStorage.getItem('CBState') || '{}');
+    // var cb = document.querySelectorAll('.checkBox');
+    // console.log(cbstate);
+    
+    
+    // let id = this.id;
+    // let todos = getTodos();
+    
+    // todos.splice(id, 1);
+    // localStorage.setItem('todo', JSON.stringify(todos));
+    
+    
+    
+    // delete cbstate[id];
+    // // localStorage.CBState = JSON.stringify(cbstate);
+    // //find a way to reset the state
+    
+    
+    
+    // localStorage.setItem("CBState", JSON.stringify(cbstate));
+    // show();
     let todos = getTodos();
-    
-    todos.splice(id, 1);
+    todos = [];
     localStorage.setItem('todo', JSON.stringify(todos));
-
+    let cbstate = JSON.parse(localStorage.getItem('CBState') || '{}');
+    cbstate = {};
+    localStorage.setItem('CBState', JSON.stringify(cbstate));
     show();
 }
 
